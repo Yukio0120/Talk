f392d99e8f6db37b9fe7063f6dad1d4f55a640f0
Lyan <dmitlyan@yandex.ru>
2022-05-13 09:33:23 +0300

    calendar done

--- app/src/main/java/com/example/talk/ui/fragments/CalendarFragment.kt
+++ app/src/main/java/com/example/talk/ui/fragments/CalendarFragment.kt
@@ -1,15 +1,13 @@
 package com.example.talk.ui.fragments
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.content.Intent
 import android.view.View
-import android.view.ViewGroup
 import android.widget.CalendarView
 import android.widget.TextView
 import com.example.talk.R
 import com.example.talk.utilits.APP_ACTIVITY
 import kotlinx.android.synthetic.main.fragment_calendar.*
+import java.util.*
 
 
 class CalendarFragment : BaseFragment(R.layout.fragment_calendar) {
